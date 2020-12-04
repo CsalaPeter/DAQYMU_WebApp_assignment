74d68575f276a522fea313f885e3afc17ec4a659
CsalaPeter <csalapeter95@gmail.com>
2020-12-04 13:24:31 +0100

    Initial commit

---
+++ assignment/src/main/java/webalk/assignment/DvdGuardianApplication.java
@@ -0,0 +1,13 @@
+package webalk.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdGuardianApplication.class, args);
+	}
+
+}
---
+++ assignment/src/test/java/webalk/assignment/DvdGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package webalk.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
