c7fb1ed580cee6d583030c5e472a40473dac7e60
CsalaPeter <csalapeter95@gmail.com>
2020-12-04 15:49:42 +0100

    Fixed project hierarchy and deleted unnecessary files.

---
+++ src/main/java/webalk/assignment/controller/DvdController.java
@@ -0,0 +1,7 @@
+package webalk.assignment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DvdController {
+}
--- src/main/java/webalk/assignment/model/Dvd.java
+++ src/main/java/webalk/assignment/model/Dvd.java
@@ -1,4 +1,4 @@
-package com.javatpoint.model;
+package webalk.assignment.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
